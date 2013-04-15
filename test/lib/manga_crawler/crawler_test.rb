78a25475e2ce8d63d7460d8172fb7af5515d93db
Thiago Rocha <kimobr@gmail.com>
2013-04-15 13:55:21 -0300

    Adding more crawler tests (get_chapters)

--- test/lib/manga_crawler/crawler_test.rb
+++ test/lib/manga_crawler/crawler_test.rb
@@ -1,10 +1,11 @@
 require "test_helper"
 
 describe MangaCrawler::Crawler do
-  
-  it "must get mangas" do
-    crawler = MangaCrawler::Crawler.new
 
+  crawler = MangaCrawler::Crawler.new
+
+  it "must get mangas" do
+    
     sample_index = File.open("test/samples/index-page.html")
     css_path = "a"
     html_field = :href
@@ -12,5 +13,16 @@ describe MangaCrawler::Crawler do
     mangas = crawler.get_mangas sample_index, css_path, nil, html_field
 
     mangas.must_equal [ ["Naruto", "/first-manga"], ["Bleach", "/second-manga"], ["One Piece", "/third-manga"] ]
+  end
+
+  it "must get chapters" do
+    
+    sample_index = File.open("test/samples/manga-page.html")
+    css_path = "a"
+    html_field = :href
+
+    chapters = crawler.get_chapters sample_index, css_path, nil, html_field
+
+    chapters.must_equal [ ["Chapter 1", "/first-manga/1"], ["Chapter 2", "/second-manga/2"], ["Chapter 3", "/third-manga/3"] ]
   end
 end
