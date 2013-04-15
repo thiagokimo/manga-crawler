28b7d130e4c745c735fdea521ef20aa2ab530644
Thiago Rocha <kimobr@gmail.com>
2013-04-15 14:43:00 -0300

    Adding crawler test 'retrieve image direct link'

--- test/lib/manga_crawler/crawler_test.rb
+++ test/lib/manga_crawler/crawler_test.rb
@@ -12,17 +12,32 @@ describe MangaCrawler::Crawler do
     
     mangas = crawler.get_mangas sample_index, css_path, nil, html_field
 
-    mangas.must_equal [ ["Naruto", "/first-manga"], ["Bleach", "/second-manga"], ["One Piece", "/third-manga"] ]
+    mangas.must_equal [ ["Naruto", "/first-manga"], 
+                        ["Bleach", "/second-manga"], 
+                        ["One Piece", "/third-manga"] ]
   end
 
   it "must get chapters" do
     
-    sample_index = File.open("test/samples/manga-page.html")
+    sample_manga_page = File.open("test/samples/manga-page.html")
     css_path = "a"
     html_field = :href
 
-    chapters = crawler.get_chapters sample_index, css_path, nil, html_field
+    chapters = crawler.get_chapters sample_manga_page, css_path, nil, html_field
 
-    chapters.must_equal [ ["Chapter 1", "/first-manga/1"], ["Chapter 2", "/second-manga/2"], ["Chapter 3", "/third-manga/3"] ]
+    chapters.must_equal [ ["Chapter 1", "/first-manga/1"], 
+                          ["Chapter 2", "/second-manga/2"], 
+                          ["Chapter 3", "/third-manga/3"] ]
+  end
+
+  it "must retrieve a direct image link from a page" do
+    
+    sample_image_page = File.open("test/samples/image-page.html")
+    css_path = "#img"
+    html_field = :src
+
+    image = crawler.get_image_from_page sample_image_page, css_path, html_field
+
+    image.must_equal "image.jpg"
   end
 end
