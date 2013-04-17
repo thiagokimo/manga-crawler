7399a1b4dafb61b3f94f37b8339881c195ab501f
Thiago Rocha <kimobr@gmail.com>
2013-04-17 12:38:26 -0300

    Adding css path in Website::Parameters

--- lib/website/parameters.rb
+++ lib/website/parameters.rb
@@ -1,11 +1,12 @@
 module Website
   class Parameters
 
-    attr_reader :base_url, :current_url
+    attr_reader :base_url, :current_url, :css_path
 
-    def initialize(base_url, current_url)
+    def initialize(base_url, current_url, css_path)
       @base_url = base_url
       @current_url = current_url
+      @css_path = css_path
     end
   end
 end
--- test/lib/website/parameters_test.rb
+++ test/lib/website/parameters_test.rb
@@ -6,7 +6,8 @@ describe Website::Parameters do
 
     base_url = "lorem.ipsum.dolor"
     current_url = "/lorem"
-    params = Website::Parameters.new(base_url, current_url)
+    css_path = "#lorem"
+    params = Website::Parameters.new(base_url, current_url, css_path)
     
     it "must have a base url" do
       params.base_url.wont_be_nil
@@ -14,6 +15,10 @@ describe Website::Parameters do
 
     it "must have a current url" do
       params.current_url.wont_be_nil
+    end
+
+    it "must have a css path" do
+      params.css_path.wont_be_nil
     end
   end
 
