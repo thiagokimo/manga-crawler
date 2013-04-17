271553b2e3559405776da79de7a2e479f84871c4
Thiago Rocha <kimobr@gmail.com>
2013-04-17 12:12:37 -0300

    Adding base url to Website::Parameters

--- lib/website/parameters.rb
+++ lib/website/parameters.rb
@@ -1,5 +1,10 @@
 module Website
   class Parameters
-  
+
+    attr_reader :base_url
+
+    def initialize(base_url)
+      @base_url = base_url
+    end
   end
 end
--- test/lib/website/parameters_test.rb
+++ test/lib/website/parameters_test.rb
@@ -1,5 +1,14 @@
 require "test_helper"
 
 describe Website::Parameters do
-  it "must have parameters"
+
+  describe "default attributes" do
+    
+    it "must have a base url" do
+      params = Website::Parameters.new("lorem.ipsum.dolor")
+      params.base_url.wont_be_nil
+    end
+
+  end
+
 end
