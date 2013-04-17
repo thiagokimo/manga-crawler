51c230e02445439dfd55308aa1c3bb372c9f1cb9
Thiago Rocha <kimobr@gmail.com>
2013-04-17 12:31:03 -0300

    Refactoring tests

--- test/lib/website/parameters_test.rb
+++ test/lib/website/parameters_test.rb
@@ -3,14 +3,16 @@ require "test_helper"
 describe Website::Parameters do
 
   describe "default attributes" do
+
+    base_url = "lorem.ipsum.dolor"
+    current_url = "/lorem"
+    params = Website::Parameters.new(base_url, current_url)
     
     it "must have a base url" do
-      params = Website::Parameters.new("lorem.ipsum.dolor", "/lorem")
       params.base_url.wont_be_nil
     end
 
     it "must have a current url" do
-      params = Website::Parameters.new("lorem.ipsum.dolor", "/lorem")
       params.current_url.wont_be_nil
     end
   end
