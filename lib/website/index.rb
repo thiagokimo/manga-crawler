334698e3cece72d74057091729bb700b3517fb14
Thiago Rocha <kimobr@gmail.com>
2013-04-17 13:12:43 -0300

    Adding params in Website:Index

--- lib/website/index.rb
+++ lib/website/index.rb
@@ -1,5 +1,11 @@
 module Website
   class Index
-  
+
+    attr_reader :params
+
+    def initialize(params)
+      @params = params
+    end
+
   end
 end
--- test/lib/website/index_test.rb
+++ test/lib/website/index_test.rb
@@ -1,5 +1,11 @@
 require "test_helper"
 
 describe Website::Index do
-  it "must have parameters"
+  it "must have parameters" do
+
+    params = Website::Parameters.new("","","","")
+    index = Website::Index.new(params)
+    
+    index.params.wont_be_nil
+  end
 end
