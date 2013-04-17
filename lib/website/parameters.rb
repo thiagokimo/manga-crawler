9b7e0c86f22264463f4ad2fe99665589ccd0d0de
Thiago Rocha <kimobr@gmail.com>
2013-04-17 12:28:20 -0300

    Adding current url in Website::Parameters

--- lib/website/parameters.rb
+++ lib/website/parameters.rb
@@ -1,10 +1,11 @@
 module Website
   class Parameters
 
-    attr_reader :base_url
+    attr_reader :base_url, :current_url
 
-    def initialize(base_url)
+    def initialize(base_url, current_url)
       @base_url = base_url
+      @current_url = current_url
     end
   end
 end
--- test/lib/website/parameters_test.rb
+++ test/lib/website/parameters_test.rb
@@ -5,10 +5,14 @@ describe Website::Parameters do
   describe "default attributes" do
     
     it "must have a base url" do
-      params = Website::Parameters.new("lorem.ipsum.dolor")
+      params = Website::Parameters.new("lorem.ipsum.dolor", "/lorem")
       params.base_url.wont_be_nil
     end
 
+    it "must have a current url" do
+      params = Website::Parameters.new("lorem.ipsum.dolor", "/lorem")
+      params.current_url.wont_be_nil
+    end
   end
 
 end
