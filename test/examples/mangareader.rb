10983597ddfffee7e594108ad7e0e163319b8bc2
Thiago Rocha <kimo@kimo.io>
2013-12-08 17:06:14 -0200

    Improving mangareader example

--- test/examples/mangareader.rb
+++ test/examples/mangareader.rb
@@ -5,7 +5,7 @@ require File.expand_path('../../../lib/manga-crawler.rb', __FILE__) #require Man
 URL = "http://www.mangareader.net"
 crawler = MangaCrawler::Crawler.new URL
 
-# mangas
+# collecting mangas
 MANGAS_URL = "http://www.mangareader.net/alphabetical"
 mangas_css = ".series_alpha li a"
 
@@ -19,9 +19,10 @@ random_manga = mangas[random_number]
 
 puts "The random manga is #{random_manga[0]}"
 
+# collecting chapters from a manga
 css_path_to_chapters = "#listing a"
 manga_url = URL + random_manga[1]
-chapters = crawler.get_chapters(manga_url, css_path_to_chapters)
+chapters = crawler.get_chapters(manga_url, css_path_to_chapters).reverse
 
 puts "#{random_manga[0]} has #{chapters.length} chapters"
 puts "Picking a random chapter..."
@@ -29,13 +30,14 @@ puts "Picking a random chapter..."
 random_chapter_number = (0..chapters.length-1).to_a.sample
 random_chapter = chapters[random_chapter_number]
 
-puts "Collecting pages from chapter #{random_chapter_number+1} of #{random_manga[0]}"
+puts "Collecting pages from chapter #{random_chapter[0]}"
 
+# collecting pages from a chapter
 chapter_url = URL + random_chapter[1]
 css_pages_path = "#pageMenu option"
 css_image_path = "img"
 pages = crawler.get_pages(chapter_url, css_pages_path, css_image_path, :src)
 
-puts "There are #{pages.length} in chapter #{random_chapter_number+1} of #{random_manga[0]}"
+puts "There are #{pages.length} pages"
 
 p pages
