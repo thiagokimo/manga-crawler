e7eaac9b67ea5979f46d67f66a06f5f92729618b
Thiago Rocha <kimo@kimo.io>
2013-12-08 19:26:30 -0200

    Now, the crawler can handle unconplete urls :)

--- lib/manga-crawler/crawler.rb
+++ lib/manga-crawler/crawler.rb
@@ -20,12 +20,15 @@ module MangaCrawler
       pages_elements = get_content(chapter_url, css_path_to_pages, :value)
 
       pages_elements.collect do |page|
-        get_content(@base_url + page[1], css_path_to_image, image_attr).first[1]
+        get_content(page[1], css_path_to_image, image_attr).first[1]
       end
     end
     
     private
     def get_content(url, css, attr)
+      
+      url = create_complete_url(url) unless url.include? @base_url
+
       html = Nokogiri::HTML(open(url))
 
       content = html.css(css)
@@ -34,6 +37,9 @@ module MangaCrawler
       end
     end
 
+    def create_complete_url(partial_url)
+      @base_url + partial_url
+    end
   end
 end
 
