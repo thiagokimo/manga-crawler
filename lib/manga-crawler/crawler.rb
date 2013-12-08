92201c69602082bbecd3143ee0abdf5f4190b02f
Thiago Rocha <kimo@kimo.io>
2013-12-08 11:59:52 -0200

    Refactoring crawler. Now, it is tiny :)

--- lib/manga-crawler/crawler.rb
+++ lib/manga-crawler/crawler.rb
@@ -1,97 +1,35 @@
 require 'nokogiri'
 require 'open-uri'
-require 'time'
 
 module MangaCrawler
   class Crawler
+    
+    def get_mangas(mangas_url, css_path_to_mangas)
+      get_content(mangas_url,css_path_to_mangas, :href)
+    end
+    
+    def get_chapters(manga_url, css_path_to_chapters)
+      get_content(manga_url, css_path_to_chapters, :href)
+    end
 
-    def get_mangas index_website
+    def get_pages(chapter_url, css_path_to_pages, css_path_to_image, image_attr)
+      pages_elements = get_content(chapter_url, css_path_to_pages, :value)
 
-      start_time = Time.now
+     pages_elements.collect do |page|
+       get_content(page[1], css_path_to_image, image_attr).first[1]
+     end
+    end
+    
+    private
+    def get_content(url, css, attr)
+      html = Nokogiri::HTML(open(url))
 
-      result = Array.new
-
-      html_index = Nokogiri::HTML(open(index_website.params.current_url))
-
-      #find all content that matches with the css_path
-      links = html_index.css(index_website.params.css_path)
-
-      #find all content from the anchor nodes found in last search
-      links.each do |anchor|
-        result.push([anchor.content, anchor[index_website.params.html_field]])
+      content = html.css(css)
+      content.collect do |e|
+        [e.text, e[attr]]
       end
-
-      #TODO
-      #if has a css_pagination, use recursion
-      #example: result += get_mangas next_link, css_path, css_pagination
-
-      end_time = Time.now
-
-      puts "\nIndex completed!"
-      puts "Elapsed time: #{end_time-start_time} seconds."
-
-      return result
     end
 
-    def get_chapters manga_website
-      #TODO
-      #uses the same logic of get_mangas
-      return self.get_mangas manga_website
-    end
-
-    def get_pages chapter_website, css_image_path
-      
-      start_time = Time.now
-
-      result = Array.new
-
-      pages_links = get_pages_links_from_chapter chapter_website
-
-      pages_links.each do |page|
-        
-        current_url = chapter_website.params.base_url + page[1]
-
-        params = Website::Parameters.new(chapter_website.params.base_url, current_url, css_image_path, :src)
-
-        result.push( self.get_image_from_page Website::Page.new(params) )
-      end
-
-      end_time = Time.now
-
-      puts "\nCollect pages completed!"
-      puts "Elapsed time: #{end_time-start_time} seconds."
-
-      return result
-
-    end
-
-    def get_pages_links_from_chapter chapter_website
-
-      result = Array.new
-
-      chapter_page = Nokogiri::HTML(open(chapter_website.params.current_url))
-
-      pages_links = chapter_page.css(chapter_website.params.css_path)
-
-      pages_links.each do |option|
-        result.push([option.content, option[chapter_website.params.html_field]])
-      end
-
-      return result
-    end
-
-    def get_image_from_page image_website
-
-      begin
-        html_image = Nokogiri::HTML(open(image_website.params.current_url))
-
-        image_link = html_image.at_css(image_website.params.css_path)[image_website.params.html_field]
-      rescue Exception => e
-        p "Error trying to access: #{image_website.params.current_url}"
-      end
-
-      return image_link
-    end
   end
 end
 
--- test/lib/manga-crawler/crawler_test.rb
+++ test/lib/manga-crawler/crawler_test.rb
@@ -1,106 +1,147 @@
-require "test_helper"
+require_relative "../../test_helper"
 
-describe MangaCrawler::Crawler do
+module MangaCrawler
+  describe Crawler do
 
-  crawler = MangaCrawler::Crawler.new
-  base_url = "localhost"
+    before do
+      @url = File.open("test/sample_website/index.html","r")
+      @sample_manga_page = File.open("test/sample_website/Bleach/bleach.html","r") 
+      @css_path_to_mangas = "a"
+      @css_path_to_chapters = ".download-link"
+      @chapter_page = File.open("test/sample_website/Bleach/chapters/1/1.html")
+      @css_path_to_pages = "#page_switch option"
+      @css_path_to_image = "#img"
+      @image_attr = :src
+    end
 
-  it "must retrieve mangas" do
+    it "must get all mangas from a website" do
+      crawler = Crawler.new
 
-    sample_index_page = File.open("test/fixtures/index.html")
-    css_path = "a"
-    html_field = :href
+      expected_manga_urls = [
+                              ["Naruto", "Naruto/naruto.html"],
+                              ["Bleach", "Bleach/bleach.html"],
+                              ["One Piece", "OnePiece/one_piece.html"]
+                            ]
 
-    params = Website::Parameters.new(base_url, sample_index_page, css_path, html_field)
-    index_page = Website::Page.new(params)
-    
-    mangas = crawler.get_mangas index_page
+      crawler.get_mangas(@url, @css_path_to_mangas).must_equal expected_manga_urls
+    end
 
-    mangas.must_equal [ ["Naruto", "Naruto/naruto.html"], 
-                        ["Bleach", "Bleach/bleach.html"], 
-                        ["One Piece", "OnePiece/one_piece.html"] ]
+    it "must retrieve the chapters of a manga page" do
+      crawler = Crawler.new
+
+      expected_chapters = [
+                              ["Bleach chapter 1", "chapters/1/1.html"],
+                              ["Bleach chapter 2", "chapters/2/1.html"],
+                              ["Bleach chapter 3", "chapters/3/1.html"]
+                            ]
+
+      crawler.get_chapters(@sample_manga_page, @css_path_to_chapters).must_equal expected_chapters
+    end
+
+    it "must retrieve the pages of a chapter" do
+      crawler = Crawler.new
+      expected_pages = ["mushroom_risotto.jpg", "vegetable_curry.jpg"]
+      
+      crawler.get_pages(@chapter_page, @css_path_to_pages, @css_path_to_image, @image_attr).must_equal expected_pages
+    end
   end
-
-  it "must retrieve chapters" do
-    
-    sample_manga_page = File.open("test/fixtures/Bleach/bleach.html")
-    css_path = ".download-link"
-    html_field = :href
-
-    params = Website::Parameters.new(base_url, sample_manga_page, css_path, html_field)
-    manga_page = Website::Page.new(params)
-
-    chapters = crawler.get_chapters manga_page
-
-    chapters.must_equal [ ["Bleach chapter 1", "chapters/1/1.html"], 
-                          ["Bleach chapter 2", "chapters/2/1.html"], 
-                          ["Bleach chapter 3", "chapters/3/1.html"] ]
-  end
-
-  it "must retrieve a direct image link from a page" do
-    
-    sample_image_page = File.open("test/fixtures/naruto/chapters/1/1.html")
-    css_path = "#img"
-    html_field = :src
-
-    params = Website::Parameters.new(base_url, sample_image_page, css_path, html_field)
-    image_page = Website::Page.new(params)
-
-    image = crawler.get_image_from_page image_page
-
-    image.must_equal "duck.jpg"
-  end
-
-  it "must retrieve all pages links from a chapter" do
-
-    sample_image_page = File.open("test/fixtures/OnePiece/chapters/2/1.html")
-    css_path = "#page_switch option"
-    html_field = :value
-
-    params = Website::Parameters.new(base_url, sample_image_page, css_path, html_field)
-    pages = Website::Page.new(params)
-    
-    pages_links = crawler.get_pages_links_from_chapter pages
-
-    pages_links.must_equal [ ["1", "1.html"], ["2", "2.html"] ]
-
-  end
-
-  it "must collect all pages from a given chapter" do
-    
-    link = "test/fixtures/Bleach/chapters/1/1.html"
-    sample_chapter_page = File.open(link)
-
-    base_url = File.absolute_path(sample_chapter_page).gsub(/test\/fixtures\/Bleach\/chapters\/1\/1.html/,"")
-
-    css_pages_path = "#page_switch option"
-    pages_html_field = :value
-
-    params = Website::Parameters.new(base_url, link, css_pages_path, pages_html_field)
-    chapter_page = Website::Page.new(params)
-
-    pages = crawler.get_pages chapter_page, "#img"
-
-    pages.must_equal ["mushroom_risotto.jpg", "vegetable_curry.jpg"]
-  end
-
-  it "must continue if some link is broken" do
-
-    link = "test/fixtures/Bleach/chapters/1/broken-1.html"
-    sample_chapter_page = File.open(link)
-
-    base_url = File.absolute_path(sample_chapter_page).gsub(/test\/fixtures\/Bleach\/chapters\/1\/broken-1.html/,"")
-
-    css_pages_path = "#page_switch option"
-    pages_html_field = :value
-
-    params = Website::Parameters.new(base_url, link, css_pages_path, pages_html_field)
-    chapter_page = Website::Page.new(params)
-
-    pages = crawler.get_pages chapter_page, "#img"
-
-    pages.must_equal [nil, "vegetable_curry.jpg"]
-  end
-
-  it "must output results in json"
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
