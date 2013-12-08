bdf24f858d860791db163d7e19a182975830328d
Thiago Rocha <kimo@kimo.io>
2013-12-08 19:27:25 -0200

    Removing base url appends from the example scripts

--- test/examples/mangareader.rb
+++ test/examples/mangareader.rb
@@ -21,8 +21,8 @@ puts "The random manga is #{random_manga[0]}"
 
 # collecting chapters from a manga
 css_path_to_chapters = "#listing a"
-manga_url = URL + random_manga[1]
-chapters = crawler.get_chapters(manga_url, css_path_to_chapters).reverse
+manga_url = random_manga[1]
+chapters = crawler.get_chapters(manga_url, css_path_to_chapters)
 
 puts "#{random_manga[0]} has #{chapters.length} chapters"
 puts "Picking a random chapter..."
@@ -33,7 +33,7 @@ random_chapter = chapters[random_chapter_number]
 puts "Collecting pages from chapter #{random_chapter[0]}"
 
 # collecting pages from a chapter
-chapter_url = URL + random_chapter[1]
+chapter_url = random_chapter[1]
 css_pages_path = "#pageMenu option"
 css_image_path = "img"
 pages = crawler.get_pages(chapter_url, css_pages_path, css_image_path, :src)
--- test/examples/starkana.rb
+++ test/examples/starkana.rb
@@ -21,7 +21,7 @@ puts "The random manga is #{random_manga[0]}"
 
 # colleting chapters from a manga
 css_path_to_chapters = ".download-link"
-manga_url = URL + random_manga[1] + "?mature_confirm=1"
+manga_url = random_manga[1] + "?mature_confirm=1"
 chapters = crawler.get_chapters(manga_url, css_path_to_chapters).reverse
 
 puts "#{random_manga[0]} has #{chapters.length} chapters"
@@ -34,7 +34,7 @@ if chapters.length > 0
   puts "Collecting pages from #{random_chapter[0]}"
 
   # collecting pages from a chapter
-  chapter_url = URL + random_chapter[1]
+  chapter_url = random_chapter[1]
   css_pages_path = "#page_switch option"
   css_image_path = "#pic img"
   pages = crawler.get_pages(chapter_url, css_pages_path, css_image_path, :src)
